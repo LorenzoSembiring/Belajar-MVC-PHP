39d8d28660492f6f54e43a25c52bc8ac929c7785
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-06-19 06:19:58 +0700

    update

--- controller/controller.php
+++ controller/controller.php
@@ -1,5 +1,5 @@
 <?php
- include("./model/model.php");
+ include("model.php");
  include("view.php");
  
 class Controller{
--- model/model.php
+++ model/model.php
@@ -2,11 +2,17 @@
 class model{
 
     function getNama(){
-           return "Novrian Rein";
+           return "Rizky Agung Prayogi";
+           return "Lorenzo Laridho Sembiring";
+           return "Fadel Muhammad";
+           return "Haikal Hanis";
       }
 
-      function getNik(){
-           return "12197525";
+      function getNim(){
+           return "1201200027";
+           return "1201200028";
+           return "1201200029";
+           return "1201200030";
       }
   
  }
