e95ecd36ae2393bc3b1155376d19cf00683d87bf
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2022-06-19 10:45:10 +0700

    Update controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -1,12 +1,14 @@
 <?php
- include("model.php");
- include("view.php");
+ include("./model/model.php");
+//  include("view.php");
+
  
 class Controller{
-function index(){
-   $data['nama']= model->getNama();
-   $data['nim']= model->getNim();
-   view->index($data);
-  }
+   function index(){
+      $model = new model;
+      $data['nama']= $model->getNama();
+      $data['nim']= $model->getNim();
+      // view->index($data);
+   }
  }
 ?> 
