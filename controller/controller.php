8e5c532bca0e8fd8aaa4485b9606acbd748f2e65
Lorenzo Laridho Sembiring <68765098+LorenzoSembiring@users.noreply.github.com>
2022-06-19 19:56:11 +0700

    Update controller.php

--- controller/controller.php
+++ controller/controller.php
@@ -1,14 +1,18 @@
 <?php
- include("./model/model.php");
-//  include("view.php");
+include("./model/model.php");
+include("./view/view.php");
 
  
 class Controller{
    function index(){
       $model = new model;
-      $data['nama']= $model->getNama();
-      $data['nim']= $model->getNim();
-      // view->index($data);
+      $view = new view;
+
+      for ($x = 0; $x <= 3; $x++) {
+         $nama= $model->getNama($x);
+         $nim= $model->getNim($x);
+         $view->index($nama, $nim);
+      }
    }
  }
 ?> 
