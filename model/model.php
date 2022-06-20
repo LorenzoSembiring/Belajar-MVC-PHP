e2222818b6d4464c9a9e81365e86ee7129f109f8
Rizky Agung Prayogi <rizkyden06@gmail.com>
2022-06-20 12:08:49 +0700

    update view

--- model/model.php
+++ model/model.php
@@ -3,20 +3,22 @@ class model
 {
      protected $nama = ['Rizky Agung Prayogi', 'Fadel Ahmad', 'Lorenzo Laridho Sembiring', 'Haikal Hanis'];
      protected $nim = [1201200027, 1201200028, 1201200023, 1201202029];
-     public function getNama()
+     public function getNama(int $i)
      {
-          return $this->nama;
+          return $this->nama[$i];
      }
 
-     public function getNim()
+     public function getNim(int $j)
      {
-          return $this->nim;
+          return $this->nim[$j];
      }
-     
-     public function setNama(string $nama){
+
+     public function setNama(string $nama)
+     {
           $this->nama = $nama;
      }
-     public function setNim(string $nim){
+     public function setNim(string $nim)
+     {
           $this->nim = $nim;
      }
 }
